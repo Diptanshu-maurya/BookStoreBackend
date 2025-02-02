2c1113a67885f007655a66a40141499089f94461
DiptanshuMaurya <diptanshum.ic.22@nitj.ac.in>
2025-02-02 18:33:38 +0530

    connection to db

--- connection/conn.js
+++ connection/conn.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose");
+require("dotenv").config();
 
 const conn=async()=>{
   try {
-    await mongoose.connect("mongodb://localhost:27017/BookStore");
+    await mongoose.connect(process.env.CONNECTION_URL);
     console.log("connected")
   } catch (error) {
      console.log(error);
